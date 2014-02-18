3730776fa864a8661c40a7b05950dc81eb918176
vl222cu <vl222cu@student.lnu.se>
2014-02-18 23:06:34 +0100

    Skapat projekt

---
+++ 1dv406_2_2_aventyrliga_kontakter/1dv406_2_2_aventyrliga_kontakter/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1dv406_2_2_aventyrliga_kontakter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 1dv406_2_2_aventyrliga_kontakter/1dv406_2_2_aventyrliga_kontakter/Model/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1dv406_2_2_aventyrliga_kontakter.Model
+{
+    public class Contact
+    {
+    }
+}
