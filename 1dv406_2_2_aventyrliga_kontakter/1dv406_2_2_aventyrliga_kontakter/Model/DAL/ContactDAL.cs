911ca08bc13243e15fdcee1ea744c273a564e58c
vl222cu <vl222cu@student.lnu.se>
2014-02-21 04:27:21 +0100

    Testar byta mapp

---
+++ 1dv406_2_2_aventyrliga_kontakter/1dv406_2_2_aventyrliga_kontakter/Model/DAL/ContactDAL.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace _1dv406_2_2_aventyrliga_kontakter.Model.DAL
+{
+    // Dataåtkomstklass
+    public class ContactDAL : DALBase
+    {
+        // Hämtar alla kontakter i databasen
+        public static IEnumerable<Contact> GetContacts()
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var contacts = new List<Contact>(100);
+
+                    var cmd = new SqlCommand("Person.uspGetContacts", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var contactIdIndex = reader.GetOrdinal("ContactID");
+                        var firstNameIndex = reader.GetOrdinal("FirstName");
+                        var lastNameIndex = reader.GetOrdinal("LastName");
+                        var emailAddressIndex = reader.GetOrdinal("EmailAddress");
+
+                        while (reader.Read())
+                        {
+                            contacts.Add(new Contact
+                            {
+                                ContactID = reader.GetInt32(contactIdIndex),
+                                FirstName = reader.GetString(firstNameIndex),
+                                LastName = reader.GetString(lastNameIndex),
+                                EmailAdress = reader.GetString(emailAddressIndex)
+                            });
+                        }
+                    }
+
+                    contacts.TrimExcess();
+
+                    return contacts;
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid kontakt med databasen.");
+                }
+            }
+        }
+
+        // Hämtar vald kontakt 
+        public static Contact GetContactById(int contactId)
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var cmd = new SqlCommand("Person.uspGetContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ContactID", contactId);
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {                    
+                        if (reader.Read())
+                        {
+                            var contactIdIndex = reader.GetOrdinal("ContactID");
+                            var firstNameIndex = reader.GetOrdinal("FirstName");
+                            var lastNameIndex = reader.GetOrdinal("LastName");
+                            var emailAddressIndex = reader.GetOrdinal("EmailAddress");
+
+                            return new Contact
+                            {
+                                ContactID = reader.GetInt32(contactIdIndex),
+                                FirstName = reader.GetString(firstNameIndex),
+                                LastName = reader.GetString(lastNameIndex),
+                                EmailAdress = reader.GetString(emailAddressIndex)
+                            };
+                        }
+                    }
+
+                    return null;
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid kontakt med databasen.");
+                }
+            }  
+        }
+
+        // Skapar ny kontakt i kontaktlistan
+        public static void InsertContact(Contact contact)
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var cmd = new SqlCommand("Person.uspAddContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@FirstName", SqlDbType.NVarChar, 50).Value = contact.FirstName;
+                    cmd.Parameters.Add("@LastName", SqlDbType.NVarChar, 50).Value = contact.LastName;
+                    cmd.Parameters.Add("@EmailAddress", SqlDbType.NVarChar, 50).Value = contact.EmailAdress;
+                    cmd.Parameters.Add("@ContactID", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+                    
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    contact.ContactID = (int)cmd.Parameters["@ContactID"].Value;
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid kontakt med databasen.");
+                }
+            }
+        }
+
+        // Uppdaterar kontakt i kontaktlistan
+        public static void UpdateContact(Contact contact)
+        { 
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var cmd = new SqlCommand("Person.uspUpdateContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@FirstName", SqlDbType.NVarChar, 50).Value = contact.FirstName;
+                    cmd.Parameters.Add("@LastName", SqlDbType.NVarChar, 50).Value = contact.LastName;
+                    cmd.Parameters.Add("@EmailAddress", SqlDbType.NVarChar, 50).Value = contact.EmailAdress;
+                    cmd.Parameters.Add("@ContactID", SqlDbType.Int, 4).Value = contact.ContactID;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid uppdateringen.");
+                }
+            }     
+        }
+
+        // Tar bort kontakt i kontaktlistan
+        public static void DeleteContact(int contactId)
+        {
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    var cmd = new SqlCommand("Person.uspRemoveContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@ContactID", SqlDbType.Int, 4).Value = contactId;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    throw new ApplicationException("Ett fel uppstod vid borttag av kontakt.");
+                }
+            }
+        }
+    }
+}
---
+++ 1dv406_2_2_aventyrliga_kontakter/1dv406_2_2_aventyrliga_kontakter/Model/DAL/DALBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace _1dv406_2_2_aventyrliga_kontakter.Model.DAL
+{
+    public abstract class DALBase
+    {
+        // Fält
+        private static string _connectionString;
+
+        // Skapar och returnerar en referens till ett
+        // anslutningsobjekt
+        protected static SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        // Kontruktor
+        static DALBase()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings["1dv406_AdventureWorksAssignmentConnectingString"].ConnectionString;
+        }
+    }
+}
